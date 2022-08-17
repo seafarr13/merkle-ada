116d1b254fe668c71e41dbbeb47d438179ab32b9
Sean Farrington <sean.farrington@merklecxm.com>
2022-08-17 13:42:43 -0500

    Added authentication on CMS and start of authorization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Merkle Accessibility",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This website contains Web Accessibility standard, best practices, and POV",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
